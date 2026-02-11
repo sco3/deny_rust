1c19fd4b33c4907e86fbe06cf1a357002ccff7ca
Dmitry Zakharov <zakharov@ibm.com>
2026-02-11 22:21:46 +0000

    compiles

--- src/deny_list_plugin.rs
+++ src/deny_list_plugin.rs
@@ -1,7 +1,32 @@
+use crate::deny_list_config::DenyListConfig;
+use aho_corasick::AhoCorasick;
+use pyo3::prelude::*;
 use pyo3::pyclass;
+use pyo3::types::{PyDict, PyDictMethods};
 
 #[pyclass(from_py_object)]
 #[derive(Clone)]
 pub struct DenyListPlugin {
-    pub deny_words: Vec<String>,
+    pub ac: AhoCorasick,
+}
+
+#[pymethods]
+impl DenyListPlugin {
+    #[new]
+    fn new(config: DenyListConfig) -> Self {
+        let ac = AhoCorasick::new(config.words.into_iter()).unwrap();
+        Self { ac }
+    }
+
+    pub fn scan(&self, args: &Bound<'_, PyDict>) -> PyResult<bool> {
+        for value in args.values() {
+            // Explicitly tell extract what type we want
+            let value_str = value.extract::<&str>()?;
+
+            if self.ac.is_match(value_str) {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
 }
